07ad5d5b67266a7cb1492f8e2701cefff76e1d83
joegagliardo <root@master-pythonml.c.rvc-instructors.internal>
2019-09-24 00:27:35 +0000

    0.3.03

--- Day3/fixhive.sh
+++ Day3/fixhive.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 mysql -ppassword -e "drop database metastore;"
 mysql -ppassword -e "create database metastore;"
+mysql -ppassword -e "grant all privileges on *.* to 'test'@'localhost' identified by 'password';"
 schematool -initSchema -dbType mysql
 
