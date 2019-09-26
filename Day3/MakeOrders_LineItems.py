8e00b4f8fb1c58a831e8b9bde0b0438c0656457b
Joseph Gagliardo <joey@me.com>
2019-09-26 18:42:03 -0400

    0.3.10

---
+++ Day3/MakeOrders_LineItems.py
@@ -0,0 +1,17 @@
+o = spark.read.csv('/home/student/ROI/SparkProgram/datasets/northwind/CSVHeaders/orders', header = True, inferSchema = True)
+od = spark.read.csv('/home/student/ROI/SparkProgram/datasets/northwind/CSVHeaders/orderdetails', header = True, inferSchema = True)
+
+o.createOrReplaceTempView('Orders')
+od.createOrReplaceTempView('OrderDetails')
+sql = """
+select o.OrderID, o.CustomerID, o.OrderDate
+           , COLLECT_SET(NAMED_STRUCT("ProductID", od.ProductID, 
+                                      "UnitPrice", od.UnitPrice,
+                                      "Quantity", od.Quantity,
+                                      "Discount", od.discount)) as LineItems
+from Orders as o join OrderDetails as od on o.OrderID = od.OrderID
+GROUP BY o.OrderID, o.CustomerID, o.OrderDate
+ORDER BY o.OrderID"""
+od2 = spark.sql(sql)
+od2.write.json('Orders_LineItems.json')
+
