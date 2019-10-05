cbdeb58db7d5935dc53383c024ad2e71eff6776b
joegagliardo <root@mckinseyinst.c.rvc-classroom.internal>
2019-10-05 03:11:36 +0000

    0.4.06

---
+++ Day4/submit.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+spark-submit --packages org.mongodb.spark:mongo-spark-connector_2.11:2.4.1,com.datastax.spark:spark-cassandra-connector_2.11:2.4.1 --conf spark.cassandra.connection.host=127.0.0.1 cassandra-mongo-pyspark.py
+
+
