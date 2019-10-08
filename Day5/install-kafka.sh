90cdc2d45740889c8fec597172fec17eee966547
joegagliardo <root@mckinseyinst.c.rvc-classroom.internal>
2019-10-08 21:14:11 +0000

    0.5.13

--- Day5/install-kafka.sh
+++ Day5/install-kafka.sh
@@ -9,3 +9,5 @@ ln -s /usr/local/kafka_2.12-2.3.0 /usr/local/kafka
 #/usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties
 #/usr/local/kafka/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic classroom
 
+mkdir /home/student/ROI/SparkProgram/Day5/stream
+
