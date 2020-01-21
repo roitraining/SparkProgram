49fbecdf4871ca2bbcce5b2ca3c9fee21478ce5a
joegagliardo <joey@me.com>
2020-01-21 16:49:37 +0000

    2.0.5

--- initspark.py
+++ initspark.py
@@ -12,11 +12,18 @@ from pyspark import SparkConf, SparkContext
 from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.types import *
 
-def initspark(appname = "Test", servername = "local", cassandra="cassandra"):
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.mongodb.spark:mongo-spark-connector_2.11:2.4.1,com.datastax.spark:spark-cassandra-connector_2.11:2.4.0 pyspark-shell'
+
+def initspark(appname = "Test", servername = "local", cassandra="127.0.0.1", mongo="mongodb://127.0.0.1/classroom"):
+    print ('initializing pyspark')
     conf = SparkConf().set("spark.cassandra.connection.host", cassandra).setAppName(appname).setMaster(servername)
     sc = SparkContext(conf=conf)
-    spark = SparkSession.builder.appName(appname).enableHiveSupport().getOrCreate()
-    sc.setLogLevel("ERROR")
+    spark = SparkSession.builder.appName(appname) \
+    .config("spark.mongodb.input.uri", mongo) \
+    .config("spark.mongodb.output.uri", mongo) \
+    .enableHiveSupport().getOrCreate()
+    sc.setLogLevel("WARN")
+    print ('pyspark initialized')
     return sc, spark, conf
 
 def hdfsPath(folder, hostname = 'localhost', port = 9000):
@@ -28,6 +35,10 @@ def hdfsPath(folder, hostname = 'localhost', port = 9000):
     if folder == None:
         folder = ''
     return 'hdfs://{0}:{1}/{2}'.format(hostname, port, folder)
+
+def display(df, limit = 10):
+    from IPython.display import display    
+    display(df.limit(limit).toPandas())
 
 if __name__ == '__main__':
     sc, spark, conf = initspark()
