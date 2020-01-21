7e8a02a5be1832b2cb2188dff3301f969e220c07
joegagliardo <joey@me.com>
2020-01-21 18:06:03 +0000

    2.0.5

--- Day5/install-kafka.sh
+++ Day5/install-kafka.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
-wget http://apache.claz.org/kafka/2.4.0/kafka_2.12-2.4.0.tgz
-tar -zxf kafka_2.12-2.4.0.tgz
+wget http://mirror.metrocast.net/apache/kafka/2.4.0/kafka_2.11-2.4.0.tgz
+tar -zxf kafka_2.11-2.4.0.tgz
 mv kafka* /usr/local
-ln -s /usr/local/kafka_2.12-2.4.0 /usr/local/kafka
+ln -s /usr/local/kafka_2.11-2.4.0 /usr/local/kafka
 wget https://github.com/Landoop/stream-reactor/releases/download/1.2.2/kafka-connect-cassandra-1.2.2-2.1.0-all.tar.gz
 tar -zxf kafka-connect-cassandra-1.2.2-2.1.0-all.tar.gz
 mv kafka-connect-cassandra-*.jar /usr/local/kafka/libs
